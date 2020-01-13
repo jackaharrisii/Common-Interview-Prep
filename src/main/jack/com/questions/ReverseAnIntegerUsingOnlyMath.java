a13e02a570dc0e0afb320fda8cbcdea5dc48e853
jackaharrisii <jackaharrisii@gmail.com>
2020-01-12 23:30:20 -0500

    added reversing an integer using only math question

---
+++ src/main/jack/com/questions/ReverseAnIntegerUsingOnlyMath.java
@@ -0,0 +1,19 @@
+package jack.com.questions;
+/*
+Given: any integer
+Task: reverse the integer using only math
+Requirements: this should work for integers >0, <0, and ==0
+ */
+
+public class ReverseAnIntegerUsingOnlyMath {
+
+    public Integer reverseInteger(Integer num){
+        Integer output = 0;
+        while(num != 0){
+            output = (output * 10) + (num % 10);
+            num /= 10;
+        }
+        return output;
+    }
+
+}
